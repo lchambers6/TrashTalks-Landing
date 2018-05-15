f89292f5f585fef80e50fbfcf20300c10463e183
Luke Chambers <lukechambers91@gmail.com>
2018-05-15 16:29:58 -0400

    added simple footer

--- trash-talks-landing/src/App.js
+++ trash-talks-landing/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Footer from "./Footer";
 import './App.css';
 
 class App extends Component {
@@ -7,15 +7,16 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Trash Talks</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Founders: Luke Chambers, Jackie Alexander, Arturo Salmeron
         </p>
+        <Footer />;
       </div>
     );
   }
 }
+
 
 export default App;
