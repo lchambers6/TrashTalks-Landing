46c47844a94cca6279879d50b4c2f73fdd7bc6dd
Luke Chambers <lukechambers91@gmail.com>
2018-05-16 10:50:25 -0400

    added icons

--- trash-talks-landing/src/components/footer/Footer.js
+++ trash-talks-landing/src/components/footer/Footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import FaInstagram from 'react-icons/lib/fa/instagram';
 import './Footer.css';
 
 class Footer extends Component {
@@ -7,11 +8,11 @@ class Footer extends Component {
       <div className="Footer">
             <div className="container">
                 <div className="row">
+                    <div className="socialMedia">
+                          <a href="https://www.instagram.com/trashtalks_welisten" target="_blank" rel="noopener noreferrer"><FaInstagram size={30} color='purple' /></a>
+                    </div>
                     <div className="copyright">
                       Copyright &copy; TrashTalks 2018
-                    </div>
-                    <div className="socialMedia">
-                          <a href="https://www.instagram.com/trashtalks_welisten" target="_blank" rel="noopener noreferrer">instagram</a>
                     </div>
                 </div>
             </div>
